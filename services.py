25bc1d518f8813fa0ce11f8877bd0fc51e86a563
Dhia Eddine Nini <dxn00@mail.aub.edu>
2021-06-26 06:21:01 +0100

    Added attributes consumption + converting frame to stream

--- services.py
+++ services.py
@@ -1,5 +1,18 @@
+from cv2 import cv2
+from io import BytesIO
+from azure.cognitiveservices.vision.face.models import FaceAttributeType
+
+
 def get_detected_faces(face_client, frame):
-    detected_faces = face_client.face.detect_with_stream(frame)
+    ret, buffer = cv2.imencode(".jpg", frame)
+    stream = BytesIO(buffer)
+
+    attributes = [FaceAttributeType.smile,
+                  FaceAttributeType.emotion]
+
+    detected_faces = face_client.face.detect_with_stream(
+        image=stream, return_face_attributes=attributes)
+
     if not detected_faces:
         raise Exception(
             'No face detected from image')
